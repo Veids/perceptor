99bbf4f0a7c5822af0b33495c7ced24452caedc9
Veids <veidsme@gmail.com>
2024-07-27 18:54:55 +0300

    fix cpp sleep block

--- pcr/codewriter/blocks/cpp/cpp_delay.py
+++ pcr/codewriter/blocks/cpp/cpp_delay.py
@@ -8,12 +8,12 @@ class cpp_delay(BaseBlock):
     yaml_tag: ClassVar[str] = "!cpp.delay"
     seconds: Annotated[int, Gt(0)]
 
-    def process(self):
+    def process(self, **kwargs):
         template = self.load_template(
             "codewriter/CPPCode/blocks",
             "delay_basic.jinja",
         )
-        return self.render_template(template, seconds=self.seconds)
+        return self.render_template(template, seconds=self.seconds, **kwargs)
 
     def info(self) -> str:
         return "Insert a delay in code"
